f930731a95b199e4bd15aacc025a926a36ca19da
Arvy Enriquez <jonarvy11@gmail.com>
2023-05-20 22:45:50 +0800

    [Misc]

--- app/src/main/java/com/sapphirex/maltool/util/Navigator.kt
+++ app/src/main/java/com/sapphirex/maltool/util/Navigator.kt
@@ -3,8 +3,15 @@ package com.sapphirex.maltool.util
 import cafe.adriel.voyager.core.screen.Screen
 import cafe.adriel.voyager.core.screen.ScreenKey
 import cafe.adriel.voyager.core.screen.uniqueScreenKey
+import cafe.adriel.voyager.navigator.Navigator
+import cafe.adriel.voyager.navigator.tab.Tab
+
+
+interface Tab : Tab {
+//    suspend fun onReselect(navigator: Navigator) {}
+}
 
 abstract class Screen : Screen {
 
-    override val key: ScreenKey = uniqueScreenKey
+//    override val key: ScreenKey = uniqueScreenKey
 }
