cee5834cb6ae76754abee167a7f1f1a60cced092
Arvy Enriquez <jonarvy11@gmail.com>
2023-05-20 17:03:46 +0800

    [Screen Implementation] MyListScreen

---
+++ app/src/main/java/com/sapphirex/maltool/ui/mylist/MyListScreen.kt
@@ -0,0 +1,12 @@
+package com.sapphirex.maltool.ui.mylist
+
+import androidx.compose.runtime.Composable
+import com.sapphirex.maltool.util.Screen
+
+object MyListScreen : Screen() {
+    @Composable
+    override fun Content() {
+
+    }
+
+}
