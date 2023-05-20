726d4f6cc716bcaee27bfa52b5b756f3a0a683a4
Arvy Enriquez <jonarvy11@gmail.com>
2023-05-20 22:45:34 +0800

    [Screen Update] Home MyList

--- app/src/main/java/com/sapphirex/maltool/ui/home/HomeScreen.kt
+++ app/src/main/java/com/sapphirex/maltool/ui/home/HomeScreen.kt
@@ -7,6 +7,8 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import cafe.adriel.voyager.navigator.LocalNavigator
+import cafe.adriel.voyager.navigator.currentOrThrow
 import com.sapphirex.maltool.ui.theme.MALToolTheme
 import com.sapphirex.maltool.util.Screen
 
@@ -14,14 +16,14 @@ object HomeScreen : Screen() {
 
     @Composable
     override fun Content() {
-        MALToolTheme {
-            // A surface container using the 'background' color from the theme
-            Surface(
-                modifier = Modifier.fillMaxSize(),
-                color = MaterialTheme.colorScheme.background
-            ) {
-                Greeting("Android")
-            }
+        val navigator = LocalNavigator.currentOrThrow
+        // A surface container using the 'background' color from the theme
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+            color = MaterialTheme.colorScheme.background
+        ) {
+            Greeting("Android")
+
         }
     }
 
--- app/src/main/java/com/sapphirex/maltool/ui/mylist/MyListScreen.kt
+++ app/src/main/java/com/sapphirex/maltool/ui/mylist/MyListScreen.kt
@@ -1,12 +1,15 @@
 package com.sapphirex.maltool.ui.mylist
 
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import com.sapphirex.maltool.util.Screen
 
 object MyListScreen : Screen() {
     @Composable
     override fun Content() {
-
+        Text(text = "MyList Screen", modifier = Modifier.fillMaxSize())
     }
 
 }
